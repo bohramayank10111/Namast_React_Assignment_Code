d0e6a1ef0c70344dbc025cb262b8e9add2b76ba9
Mayank Bohra <mayankbohra12@gmail.com>
2025-09-04 21:34:28 +0530

    assignment 1 namaste react

---
+++ index.js
@@ -0,0 +1,12 @@
+const anotherParent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", { id: "heading1" }, "Heading 1"),
+React.createElement("h1", { id: "heading1" }, "Heading 2"),
+
+]),
+// Another children
+React.createElement("div", { id: "child2" }, [React.createElement("h1", { id: "heading3" }, "Heading 3"),
+React.createElement("h1", { id: "heading4" }, "Heading 4"),
+])
+])
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(anotherParent);
